06ed57c1f56cadba27ac0ce55bc9d98caf28b789
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-03-10 16:32:11 -0300

    commit exe007

---
+++ Exercícios/exe007estruturaRepeticaoFor.js
@@ -0,0 +1,12 @@
+var i;
+var carros = ["Sandeiro", "Gol", "Polo", "Corola", "Savero"];
+
+for(i = 0; i <= 10; i++){
+   // console.log(i);
+}
+for(i=0; i < carros.length; i++){
+   // console.log(carros[i]);
+}
+for(i = carros.length-1; i >= 0; i--){
+    console.log(carros[i]);
+}
